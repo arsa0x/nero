279f0a03e10a866d4771d5c1c0a16dfe55f1775d
arsa0x <idunvoneinzbern@gmail.com>
2025-12-15 21:24:56 +0700

    feat: adding resolver

--- nero_bin/src/main.rs
+++ nero_bin/src/main.rs
@@ -1,6 +1,7 @@
 use nero_core::{
     lexer::Lexer,
     parser::{Parser, ParserError},
+    resolver::Resolver,
 };
 
 const EXAMPLE: &str = include_str!("../../example/get_method.ns");
@@ -11,8 +12,19 @@ fn main() -> Result<(), ParserError> {
 
     match lex {
         Ok(result) => {
-            let mut parser: Parser = Parser::new(result);
-            let _ = parser.parse()?;
+            let mut parser = Parser::new(result);
+            let ast = parser.parse()?;
+
+            let mut resolver = Resolver::new();
+
+            for stmt in &ast {
+                match resolver.resolve_statement(stmt) {
+                    Ok(_) => {
+                        println!("{:?}", resolver.variables);
+                    }
+                    Err(e) => println!("{:?}", e),
+                }
+            }
         }
         Err(err) => println!("{:?}", err),
     }
---
+++ nero_core/src/resolver.rs
@@ -0,0 +1,67 @@
+use std::collections::HashMap;
+
+use crate::ast::{Expr, Stmt, StringPart};
+
+pub struct Resolver {
+    pub variables: HashMap<String, Value>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Value {
+    Number(i64),
+    String(String),
+}
+
+#[derive(Debug)]
+pub enum ResolveError {
+    UndefinedVariable(String),
+    InvalidExpression,
+}
+
+impl Resolver {
+    pub fn new() -> Self {
+        Self {
+            variables: HashMap::new(),
+        }
+    }
+    fn resolve_expression(&self, expr: &Expr) -> Result<Value, ResolveError> {
+        match expr {
+            Expr::Number(n) => Ok(Value::Number(*n)),
+            Expr::Identifier(name) => self
+                .variables
+                .get(name)
+                .cloned()
+                .ok_or(ResolveError::UndefinedVariable(name.clone())),
+            Expr::String(parts) => {
+                let mut result = String::new();
+
+                for part in parts {
+                    match part {
+                        StringPart::Text(t) => result.push_str(t),
+                        StringPart::Expression(e) => {
+                            let v = self.resolve_expression(e)?;
+                            match v {
+                                Value::String(s) => result.push_str(&s),
+                                Value::Number(n) => result.push_str(&n.to_string()),
+                            }
+                        }
+                    }
+                }
+                Ok(Value::String(result))
+            }
+        }
+    }
+    pub fn resolve_statement(&mut self, stmt: &Stmt) -> Result<(), ResolveError> {
+        match stmt {
+            Stmt::Assignment { name, value } => {
+                let v = self.resolve_expression(value)?;
+                self.variables.insert(name.clone(), v);
+                Ok(())
+            }
+            Stmt::Request(req) => {
+                let _url = self.resolve_expression(&req.url)?;
+                Ok(())
+            }
+        }
+    }
+}
